7d60de7c790c7b054541d3f0fed3adcd2784a697
farhadabdillahd-maker <farhadabdillahd@gmail.com>
2026-07-26 23:12:53 +0700

    Update pdf_report.py

--- modules/pdf_report.py
+++ modules/pdf_report.py
@@ -51,7 +51,7 @@ def generate_pdf(
 
     elements = []
 
-    logo_polri = Image(os.path.join("asset","logo_polri.png"), width=2.8*CM, height=2.8*CM)
+    logo_polri = Image(os.path.join("asset","logo_polri.jpg"), width=2.8*CM, height=2.8*CM)
     logo_polda = Image(os.path.join("asset","logo_polda.png"), width=2.8*CM, height=2.8*CM)
 
     style_kop = ParagraphStyle(
